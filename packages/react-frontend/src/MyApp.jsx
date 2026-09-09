4009567991ca6af07d70629c8803182403915123
sainakakkar2006 <skakkar@calpoly.edu>
2026-09-08 21:28:09 -0700

    Load users from backend in React

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,10 +1,20 @@
 // src/MyApp.jsx
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Table from "./Table";
 import Form from "./Form";
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
+    function fetchUsers() {
+    return fetch("http://localhost:8000/users");
+  }
+
+  useEffect(() => {
+    fetchUsers()
+      .then((response) => response.json())
+      .then((users) => setCharacters(users))
+      .catch((error) => console.error(error));
+  }, []);
 
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
