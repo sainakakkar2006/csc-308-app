3eb7303ffb2f36572c2fb0e2c172e85591fa1227
saina kakkar <skakkar@calpoly.edu>
2026-09-25 00:05:22 -0700

    Restore job validation and users collection

--- packages/expressjs-backend/user.js
+++ packages/expressjs-backend/user.js
@@ -1,16 +1,27 @@
 import mongoose from "mongoose";
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    job: {
+      type: String,
+      required: true,
+      trim: true,
+      validate(value) {
+        if (value.length < 2) {
+          throw new Error(
+            "Invalid job, must be at least 2 characters."
+          );
+        }
+      },
+    },
   },
-  job: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-});
+  { collection: "users_list" }
+);
 
 const User = mongoose.model("User", userSchema);
 
